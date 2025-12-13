d890e41463b85e0a552f07076f3f6017c45261d9
Clevison <cleverson-altF4>
2025-12-13 02:51:33 -0500

    ternário

---
+++ Módulo 1/aula-1.py
@@ -0,0 +1,12 @@
+#Iniciante.
+#tempo = int(input("Quantos anos de uso seu carro tem? "))
+#if tempo <= 3:
+#    print("Seu carro está novo.")
+#else:
+#    print("Seu carro está velho.")
+
+
+#simplificada
+tempo = int(input("Quantos anos de uso tem o seu carro? "))
+print("carro novo"if tempo <= 3 else 'carro velho') 
+#ternário
