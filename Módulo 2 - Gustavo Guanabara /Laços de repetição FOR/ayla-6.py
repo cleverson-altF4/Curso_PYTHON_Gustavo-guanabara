cb60e8667fbf47460fb854a54d7b39f4a0665021
Clevison <cleverson-altF4>
2025-12-14 00:50:29 -0500

    uma progressao aritmética usando for

---
+++ Módulo 2 - Gustavo Guanabara /Laços de repetição FOR/ayla-6.py
@@ -0,0 +1,6 @@
+primeiro_termo = int(input("Primeiro termo: "))
+razao = int(input("De quanto em quanto? "))
+P_a = primeiro_termo + (10 - 1) * razao
+
+for i in range(primeiro_termo, P_a + razao, razao):
+    print(f"{i} - ", end= '')
