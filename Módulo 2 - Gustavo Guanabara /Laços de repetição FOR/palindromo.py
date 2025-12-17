a4d9e760b3b54f3b2ef00c7f33825167b10f4922
Clevison <cleverson-altF4>
2025-12-16 22:56:23 -0500

    melhorias

--- Módulo 2 - Gustavo Guanabara /Laços de repetição FOR/palindromo.py
+++ Módulo 2 - Gustavo Guanabara /Laços de repetição FOR/palindromo.py
@@ -3,10 +3,11 @@ palavra = frase.split()
 junto = ''.join(palavra)
 inverso = ''
 
-for letra in range(len(junto)-1, -1, -1):
+for letra in range(len(junto)-1,-1,-1):
     inverso += junto[letra]
 
-print(inverso)
+print("Junto: {}".format(junto))
+print("Inverso: {}".format(inverso))
 if inverso == junto:
     print("É um palíndromo")
 else:
