8e85713b303894f871fb66f4ed4646bb6f70c371
Clevison <cleverson-altF4>
2025-12-26 19:49:40 -0500

    correção com o meu código

--- Módulo 2 - Gustavo Guanabara /Laços de repetição FOR/analise-de-dados.py
+++ Módulo 2 - Gustavo Guanabara /Laços de repetição FOR/analise-de-dados.py
@@ -1,16 +1,17 @@
 media = 0
-soma_idade = 0
+media_idade = 0
 maior_idade_homem = 0
 nome_velho = ''
 total_mulher = 0
+soma_idade = 0
 
 for p in range(1,5):
-    print("#"*10, " {}° - Pessoa ".format(p),"#"*10)
-    nome = str(input("Nome: ")).strip()
-    idade = int(input("Idade: "))
-    sexo = str(input("M/F: ")).strip().upper()
+    print("*"* 10, "Maioridade de idade", "*"* 10, "\n")
+    nome = str(input("Digite o seu nome: ")).strip().upper()
+    idade = int(input("Sua idade: "))
+    sexo = str(input("Sexo M/F: ")).strip()
     soma_idade += idade
-    
+
     if p == 1 and sexo in 'Mm':
         maior_idade_homem = idade
         nome_velho = nome
@@ -21,9 +22,6 @@ for p in range(1,5):
         total_mulher += 1
 
 media = soma_idade / 4
-print("A média de idade do grupo é de {} anos.".format(media))
-print("O homem mais velho do grupo tem {} anos e se chama {}".format(maior_idade_homem, nome_velho))
-print("Ao total de mulheres com menos de 20 anos são ao total de {} ".format(total_mulher))
-
-
-
+print(f"A média da idade do grupo é de {media} anos")
+print(f"A pessoa mais velha do grupo é {maior_idade_homem} anos e o nome dele é {nome_velho}")
+print("Ao total são {} mulheres com menos de 20 anos".format(total_mulher))
