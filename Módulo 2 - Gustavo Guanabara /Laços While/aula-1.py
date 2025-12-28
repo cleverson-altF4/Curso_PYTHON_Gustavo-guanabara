032afca32cfb37f14217917d589a0ff08b7816ee
Clevison <cleverson-altF4>
2025-12-28 15:05:50 -0500

    usando contagem de números pares e ímpares

--- Módulo 2 - Gustavo Guanabara /Laços While/aula-1.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/aula-1.py
@@ -1,6 +1,11 @@
 valor = 's'
+par = impar = 0
 while valor == 's':
     numero = int(input("Digite um valor: "))
-    valor = str(input("quer continuar s/n: ")).lower()
-    
-print("Fim")
+    valor = str(input("\nquer continuar s/n: ")).lower()
+    if numero % 2 == 0:
+        par += 1
+    else:
+        impar += 1
+print(f"Números pares {par}")
+print(f"Números ímpares {impar}")
