7a2b4aa4e6a29bee1ffc8fdc2b298fd787ffff84
Clevison <cleverson-altF4>
2025-12-31 20:15:08 -0500

    correção do prof: Guanabara

--- Módulo 2 - Gustavo Guanabara /Laços While/desafio-1.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/desafio-1.py
@@ -1,16 +1,9 @@
 #Faça um programa que leia o sexo, mas so aceite os valores "M" ou "F", caso contrário
 #peça a digitação novamente até ter um valor correto
 
-sexo = str(input("Digite o seu sexo M/F: ")).strip().lower()
+sexo = str(input("Digite o seu sexo M/F: ")).strip().upper()[0]
+print(sexo)
+while sexo not in 'MfFm' :
+    sexo = str(input("Dados inválidos, digite novamente: ")).strip().upper()[0]
+print(f"Sexo {sexo} registrado com sucesso!")
 
-while sexo not in ['m', 'f']:
-    print("Erro de digitação, tente novamente\n")
-    print("Use apenas M/F\n")
-    sexo = str(input("digite novamente: "))
-    
-   
-    
-if sexo in 'm':
-    print("Você é homem")
-else:
-    print("Você é mulher")
