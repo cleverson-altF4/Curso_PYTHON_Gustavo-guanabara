d936e5bf93ff9a27cca8fbc87b9142b1ec3a5be7
Clevison <cleverson-altF4>
2026-01-02 23:09:34 -0500

    melhorias na validação de dados

--- Módulo 2 - Gustavo Guanabara /Laços While/desafio-1.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/desafio-1.py
@@ -1,9 +1,26 @@
 #Faça um programa que leia o sexo, mas so aceite os valores "M" ou "F", caso contrário
 #peça a digitação novamente até ter um valor correto
 
-sexo = str(input("Digite o seu sexo M/F: ")).strip().upper()[0]
-print(sexo)
-while sexo not in 'MfFm' :
-    sexo = str(input("Dados inválidos, digite novamente: ")).strip().upper()[0]
-print(f"Sexo {sexo} registrado com sucesso!")
+contador = 0
 
+while True:
+    entrada = str(input("Digite seu sexo [M] ou [F]: ")).strip()
+    if not entrada:
+        print("entrada vazia. tente novamente")
+        contador += 1
+        continue
+        
+    sexo = entrada[0].upper()
+    
+    if sexo in ('M', 'F'):
+        break
+    print("Dados inválidos, digite apenas 'M ou F")
+    contador += 1
+        
+        
+print(f"Sexo [{sexo} registrado]")
+if contador:
+    print(f"Teve {contador} tentativas")
+
+    
+        
