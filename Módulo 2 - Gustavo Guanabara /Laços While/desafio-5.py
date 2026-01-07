56417b43baeb66b28480fb1bc6a971c9c065b600
Clevison <cleverson-altF4>
2026-01-07 17:48:37 -0500

    update PA

--- Módulo 2 - Gustavo Guanabara /Laços While/desafio-5.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/desafio-5.py
@@ -1,22 +1,9 @@
+primeiro_termo = int(input("Progressão aritmética: "))
+razao = int(input("Razão: "))
+termo = primeiro_termo
+contador = 1
 
-while True:
-    primeiro_termo = int(input("Digite o primeiro termo da PA: "))
-    razao = int(input("Digite a razão: "))
-    
-    termo_atual = primeiro_termo
-    contador = 0 # zero
-    
-    print(f"os 10 primeiro termos são = {termo_atual} números da PA", end= ' ')
-    contador += 1 # conto
-    
-    
-    while contador < 10:
-        termo_atual = termo_atual + razao
-        contador += 1
-        print(f"{termo_atual}", end= ' ')
-    print('\n')
-        
-    
-    continuar = input("Deseja continuar: S/N: ").strip().lower()
-    if continuar == 'n':
-        break
+while contador <= 10:
+    print(f"{termo} -> ", end= ' ')
+    termo = termo + razao
+    contador += 1
