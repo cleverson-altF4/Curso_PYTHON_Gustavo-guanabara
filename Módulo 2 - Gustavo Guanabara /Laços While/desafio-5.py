bcd5f7534e53f8a289a108b0942d6630a291bf7c
Clevison <cleverson-altF4>
2026-01-07 23:44:25 -0500

    update PA

--- Módulo 2 - Gustavo Guanabara /Laços While/desafio-5.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/desafio-5.py
@@ -1,9 +1,24 @@
-primeiro_termo = int(input("Progressão aritmética: "))
-razao = int(input("Razão: "))
-termo = primeiro_termo
-contador = 1
-
-while contador <= 10:
-    print(f"{termo} -> ", end= ' ')
-    termo = termo + razao
-    contador += 1
+while True:
+    primeiro_termo = int(input("Progressão aritmética: "))
+    razao = int(input("Razão: "))
+    contador = 1
+    
+    termo = primeiro_termo
+    contador = 0
+    
+    print(f"{termo}")
+    
+    while contador < 10:
+        termo = termo + razao
+        contador += 1
+        print(f"{termo}", end= ' - ')
+          
+    print("\n")
+    
+    continuar = str(input("Deseja continuar? S/N ")).strip().upper()
+    
+    if continuar == 'N':
+        print("Foram contadas {} vezes".format(contador))
+        break
+    
+    
