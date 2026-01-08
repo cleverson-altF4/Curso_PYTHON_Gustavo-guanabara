1ff496e3c81238f1ca1fac76202a8923e7eb3697
Clevison <cleverson-altF4>
2026-01-08 15:38:29 -0500

    update

--- Módulo 2 - Gustavo Guanabara /Laços While/desafio-6.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/desafio-6.py
@@ -1,17 +1,16 @@
 #escreva um programa que leia um número N qualquer e mostre na tela os n primeiros elementos de uma sequência de Fibonacci
 
-#F(0) = 0
+#F(a) = 0
 #F(1) = 1
 # F(n) = F(n−1) + F(n−2)
 
 #a, b = b, a + b   
 
-a = 0
-b = 1
+a, b = 0, 1
 contador = 0
-n = int(input("Fibonacci: "))
+numero = int(input("Sequência de Fibonacci: "))
 
-while contador < n:
-    print(a, end= ' ')
-    a, b = b, a + b
+while contador < numero:
+    print(a, end= ' - ')
+    a,b = b, a + b
     contador += 1
