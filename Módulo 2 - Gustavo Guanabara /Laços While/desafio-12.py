3869b62423b7c4a613563ba6ae22ddc3f37b27ff
Clevison <cleverson-altF4>
2026-01-10 01:48:49 -0500

    correção

--- Módulo 2 - Gustavo Guanabara /Laços While/desafio-12.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/desafio-12.py
@@ -1,6 +1,21 @@
 soma = subtracao = multi = divisao = resultado = 0
 while True:
     tabuada = int(input("Tabuada: "))
-    resultado = soma + tabuada
-    for tabuada in range(11):
-        print(f"{tabuada} + {soma} = {resultado}")
+    
+    for i in range(11):
+        soma = tabuada + i
+        resultado = soma
+        print(f"{tabuada} + {i} = {resultado}")
+    
+    print("\nDeseja continuar? \n")
+    continuar = int(input("Sim/Não: ")).strip().upper()[0]
+    if continuar == 'S':
+        continue
+    else:
+        break
+        
+        
+    
+
+        
+        
