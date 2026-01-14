0caa438affc6f046f2a2aad86702c343187d7cd7
Clevison <cleverson-altF4>
2026-01-14 16:33:52 -0500

    update

--- Módulo 2 - Gustavo Guanabara /Laços While/desafio-14.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/desafio-14.py
@@ -6,43 +6,43 @@
 quantidade_pessoas = 0
 quantidade_homens = 0
 quantidade_mulheres = 0
-mais_18 = 0
+quantidade_mais18 = 0
+print('*'*75)
+print()
+print('*'*25, "Validação de idades", '*'*29)
+print()
+print('*'*75)
 
-print("-"*50)
-print('*'*18, "Fim do Programa", '*'*15)
-print("-"*50)
 
 while True:
-    idade = int(input("Idade: "))
-    sexo = str(input("Sexo [M/F]: ")).strip().upper()[0]
+    idade = int(input("Digite a sua idade: "))
+    sexo = str(input("Digite o seu sexo [M/F]: ")).strip().upper()
     
     if sexo not in ('M', 'F'):
-        print("-"*50)
-        print("\nNão é permitido. Tente apenas [M ou F]")
-        print("-"*50)
+        print('*'*75)
+        print("\n\033[31mErro!\033[mDados incorretos, tente novamente\n")
+        print('*'*75)
         continue
     
     quantidade_pessoas += 1
     
     if idade > 18:
-        mais_18 += 1
-        
+        quantidade_mais18 += 1
+    
     if sexo == 'M':
         quantidade_homens += 1
         
     if sexo == 'F' and idade < 20:
         quantidade_mulheres += 1
         
-    
-    continuar = input("Quer continuar? [Sim ou Não]: ").strip().upper()[0]
+    continuar = str(input("Deseja continuar? [Sim ou Não]: ")).strip().upper()[0]
     if continuar == 'N':
         break
+
     
 print("-"*50)
 print(f"Total de pessoas cadastradas: {quantidade_pessoas}")
-print(f"Pessoas com mais de 18 anos: {mais_18}")
+print(f"Pessoas com mais de 18 anos: {quantidade_mais18}")
 print(f"Homens cadastrados: {quantidade_homens}")
 print(f"Mulheres com menos de 20 anos: {quantidade_mulheres}")
 print("-"*50)
-
-    
