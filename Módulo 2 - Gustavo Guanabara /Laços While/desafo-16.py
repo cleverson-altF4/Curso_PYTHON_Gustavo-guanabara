f0a7dd7878c9f8c69f1114c2e620af55c68a6df6
Clevison <cleverson-altF4>
2026-01-15 17:38:32 -0500

    update now

--- Módulo 2 - Gustavo Guanabara /Laços While/desafo-16.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/desafo-16.py
@@ -3,16 +3,17 @@ print("*"*50)
 print('~'* 15, "Banco central", '~'* 15)
 print("*"*50)
 
-dinheiro = int(input("Sacar valor R$: "))
+dinheiro = int(input("Valor R$: "))
 total = dinheiro
 cedulas = 50
 total_cedulas = 0
+            
 while True:
     if total >= cedulas:
         total -= cedulas
         total_cedulas += 1
     else:
-        print(f"Total de {total_cedulas} células de R$ {cedulas}")
+        print(f"{total_cedulas} de R$ {cedulas}")
         if cedulas == 50:
             cedulas = 20
         elif cedulas == 20:
