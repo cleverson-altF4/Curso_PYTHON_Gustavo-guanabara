5d295e10527015b6382c259f00f9ce4df550f3a2
Clevison <cleverson-altF4>
2026-01-15 15:17:30 -0500

    Update Guanabara

--- Módulo 2 - Gustavo Guanabara /Laços While/desafio-14.py
+++ Módulo 2 - Gustavo Guanabara /Laços While/desafio-14.py
@@ -2,47 +2,48 @@
 
 # a - Quantas pessoas tem mais de 18 anos
 # b - Quantos homens foram cadastrados
-# c - Quantas mulheres tem menos de 20 anos
-quantidade_pessoas = 0
-quantidade_homens = 0
-quantidade_mulheres = 0
-quantidade_mais18 = 0
+# c - Quantas mulheres tem menos de 20 anoswh
+total18 = 0
+total_homens = 0
+total_mulheres = 0
 print('*'*75)
-print()
 print('*'*25, "Validação de idades", '*'*29)
-print()
-print('*'*75)
-
-
 while True:
-    idade = int(input("Digite a sua idade: "))
-    sexo = str(input("Digite o seu sexo [M/F]: ")).strip().upper()
-    
-    if sexo not in ('M', 'F'):
-        print('*'*75)
-        print("\n\033[31mErro!\033[mDados incorretos, tente novamente\n")
-        print('*'*75)
-        continue
-    
-    quantidade_pessoas += 1
-    
+    idade = int(input("Idade: "))
+    sexo = ' '
+    while sexo not in ('M', 'F'):
+        sexo = str(input("Sexo [M/F]: ")).strip().upper()
+        if sexo == '':
+            print("Apenas [M/F]")
     if idade > 18:
-        quantidade_mais18 += 1
-    
+        total18 += 1   
+        
     if sexo == 'M':
-        quantidade_homens += 1
+        total_homens += 1
         
-    if sexo == 'F' and idade < 20:
-        quantidade_mulheres += 1
-        
-    continuar = str(input("Deseja continuar? [Sim ou Não]: ")).strip().upper()[0]
+    if idade < 20 and sexo == 'F':
+        total_mulheres += 1
+    
+            
+    continuar = ''
+    while continuar not in ('S', 'N'):
+        continuar = input("Deseja continuar? [S/N]: ").strip().upper()
+        if continuar == '':
+            print("Erro! Digite S ou N.")
+        else:
+            continuar = continuar[0]          
     if continuar == 'N':
         break
 
+print(f"Total de pessoas com mais de 18 anos = {total18}")
+print(f"Total de homens cadastrados = {total_homens}")
+print(f"total de mulheres com menos de 20 anos = {total_mulheres}")
     
-print("-"*50)
-print(f"Total de pessoas cadastradas: {quantidade_pessoas}")
-print(f"Pessoas com mais de 18 anos: {quantidade_mais18}")
-print(f"Homens cadastrados: {quantidade_homens}")
-print(f"Mulheres com menos de 20 anos: {quantidade_mulheres}")
-print("-"*50)
+
+
+
+
+
+
+
+    
