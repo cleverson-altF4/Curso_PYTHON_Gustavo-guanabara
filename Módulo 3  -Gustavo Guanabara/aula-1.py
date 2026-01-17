8b2f99400be41b16f70e604ce4660564cf4361af
Clevison <cleverson-altF4>
2026-01-17 10:28:00 -0500

    Desafio - 1

---
+++ Módulo 3  -Gustavo Guanabara/aula-1.py
@@ -0,0 +1,3 @@
+numero = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' )
+
+print(numero[15])
