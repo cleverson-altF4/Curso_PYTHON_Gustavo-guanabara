50afac18750b215e691f659ab68592c9b4e2808c
Clevison <cleverson-altF4>
2026-01-17 15:15:47 -0500

    update

--- Módulo 3  -Gustavo Guanabara/aula-1.py
+++ Módulo 3  -Gustavo Guanabara/aula-1.py
@@ -1,3 +1,8 @@
-numero = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' )
-
-print(numero[15])
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' )
+while True:
+    numero = int(input("Digite um número : [0 à 20]: "))
+    if 0 <= numero <= 20:
+        break
+    else:
+        print("Tente novamente")
+print(f"Numero = {numeros[numero]}")
