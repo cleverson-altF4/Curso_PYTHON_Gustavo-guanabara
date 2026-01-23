a9ffee09ffe06aa106311db268fbf8348185976f
Clevison <cleverson-altF4>
2026-01-22 22:55:02 -0500

    exclusão

--- Módulo 3  -Gustavo Guanabara/aula-1.py
+++
@@ -1,9 +0,0 @@
-numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' )
-while True:
-    tabela = int(input("Digite um número de [0 à 20]: "))
-    if 0 <= tabela <=20:
-        break
-    else:
-        print("Tente novamente")
-
-print(f"Você digitou o número: {numeros[tabela]}")
