005ca0e2825dbb46599d285380ff7edec62f91b3
Clevison <cleverson-altF4>
2026-01-24 11:01:10 -0500

    update

--- Módulo 3  -Gustavo Guanabara/Listas/aula-3.py
+++ Módulo 3  -Gustavo Guanabara/Listas/aula-3.py
@@ -10,3 +10,10 @@ print("*"* 30)
 
 for i, v in enumerate(valores): # Ou usar com uma numeração e saber a posição de cada
     print(f"posição {i} - {v}")
+
+print("*"*30)
+
+for contador in range(0,5): # ou posso usar esse usando uma quantidade de vezes adicionado em valores vazios dentro do for e usando input
+    valores.append(int(input("Digite um valor: [1 à 5]: ")))
+    print(f"Valores adicionados {valores}")
+
