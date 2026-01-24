a4cfe81b936c29b332cb0c0a4609e35a7ed685e6
Clevison <cleverson-altF4>
2026-01-24 10:41:41 -0500

    update

--- Módulo 3  -Gustavo Guanabara/Listas/aula-1.py
+++ Módulo 3  -Gustavo Guanabara/Listas/aula-1.py
@@ -1,4 +1,10 @@
 numero = [1,2,3,4,5,6]
 numero[3] = 3
-numero.append(7) # adicionando um numero usando append(7) e ele ficará na última posição
+numero.append(4) # adicionando um numero usando append(7) e ele ficará na última posição
+numero.sort() # números alinhados na ordem correta
+numero.sort(reverse=True) # números de trás para frente
+numero.insert(0,0) # O insert você escolhe a posição de onde quer deixar e adiciona um valor desejado que foi o 0
+numero.pop() # ele irá apagar apenas o último número
+numero.pop(3) # ele irá apagar o número 3 da lista
 print(numero)
+print(f"Essa lista tem {len(numero)} elementos")
