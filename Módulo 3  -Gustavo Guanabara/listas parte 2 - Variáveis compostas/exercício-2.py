ad6588e4fea1d4b203f0959744867b8c5c233c55
Clevison <cleverson-altF4>
2026-02-07 19:23:21 -0500

    índices[0][1]

---
+++ Módulo 3  -Gustavo Guanabara/listas parte 2 - Variáveis compostas/exercício-2.py
@@ -0,0 +1,8 @@
+galera = [["Clevison", 31], ["Claudia", 30], ["Cuca", 25]]
+print(galera[0][0]) #Clevison
+print(galera[1][0]) #claudia
+print(galera[2][0]) #Cuca
+print(galera[:]) #[["Clevison", 31], ["Claudia", 30], ["Cuca", 25]]
+print(galera[0][1]) #31
+print(galera[1][1]) #30
+print(galera[2][1]) #25
