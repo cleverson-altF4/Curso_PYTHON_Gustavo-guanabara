04b5ab70c652de031d2b61f04d2c6b0d5c53f0b4
Clevison <cleverson-altF4>
2026-02-09 15:40:27 -0500

    update

--- Módulo 3  -Gustavo Guanabara/listas parte 2 - Variáveis compostas/desafio-1.py
+++ Módulo 3  -Gustavo Guanabara/listas parte 2 - Variáveis compostas/desafio-1.py
@@ -1,53 +1,52 @@
 lista_pessoas = list()
-dados_da_lista = list()
+dados_lista = list()
 pessoas_cadastradas = 0
-maior_peso = menor_peso = 0
-
+maior = menor = 0
 
 while True:
-    dados_da_lista.append(str(input("Digite o seu nome: ")))
-    dados_da_lista.append(float(input("Digite o seu peso: ")))
-    lista_pessoas.append(dados_da_lista[:])
-    dados_da_lista.clear()
+    dados_lista.append(str(input("Digite o seu nome: ")))
+    dados_lista.append(float(input("Digite o seu peso: ")))
+    lista_pessoas.append(dados_lista[:])
+    dados_lista.clear()
     
-    peso = lista_pessoas[-1][1]
-    if len(lista_pessoas) == 1:
-        maior_peso = menor_peso = peso
+    peso = lista_pessoas[-1][1] # Pega a última pessoa da lista até o primeiro
+    
+    if len(lista_pessoas) == 1: #Se a quantidade de pessoas da lista for até o 1
+        maior = menor = peso  #maior peso, menor peso e peso
     else:
-        if peso > maior_peso:
-            maior_peso = peso
+        if peso > maior:
+            maior = peso
         
-        if peso < menor_peso:
-            menor_peso = peso            
-    
+        if peso < menor:
+            menor = peso
+            
+            
     continuar = ''
     while continuar not in ('S', 'N'):
-        continuar = str(input("Deseja continuar? [S/N]: ")).strip().upper()
+        continuar = str(input("deseja continuar? [Sim/Não]: ")).strip().upper()
         if continuar == '':
-            print("Não é permitido espaço vazio")
+            print("Não é permitido espaços vazios")
         else:
             continuar = continuar[0]
     if continuar == 'N':
         break
     
 print("-"*40)
-    
+
 pessoas_cadastradas = len(lista_pessoas)
-print("\nTotal de pessoas cadastradas:", pessoas_cadastradas)
-print("Maior peso:", maior_peso, "kg")
-print("Menor peso:", menor_peso, "kg")
+print(f"Temos {pessoas_cadastradas} pessoas cadastradas")
+print(f"O maior peso = {maior}")
+print(f"O menor peso = {menor}")
 
 print("-"*40)
-#laço percorre a lista de pessoas maior peso       
-for i in lista_pessoas:
-    if i[1] == maior_peso:
-       print(f"{i[0]} tem {i[1]} kg")
-            
-    
-#Laço percorre a lista de pessoas com menor peso
-for i in lista_pessoas:
-    if i[1] == menor_peso:
-        print(f"{i[0]} tem {i[1]} kg")
 
-    
-    
+for i in lista_pessoas:
+    if i[1] == maior:
+        print(f"{i[0]} tem o maior peso que é {maior} kg")
+        
+        
+for i in lista_pessoas:
+    if i[1] == menor:
+        print(f"{i[0]} tem o menor peso que é {menor} kg")
+        
+print("-"*40)
