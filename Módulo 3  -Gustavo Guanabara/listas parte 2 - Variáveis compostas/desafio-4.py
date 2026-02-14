871e57c10bb8f8d14692649fe8d663e9e1706f7b
Clevison <cleverson-altF4>
2026-02-14 14:29:46 -0500

    update

--- Módulo 3  -Gustavo Guanabara/listas parte 2 - Variáveis compostas/desafio-4.py
+++ Módulo 3  -Gustavo Guanabara/listas parte 2 - Variáveis compostas/desafio-4.py
@@ -2,31 +2,63 @@
 
 #aprimore o desafio anterior mostrando no final: 
 #A soma de todos os valores pares digitados
-#A soma dos valores da terceira coluna
-#O maior valor da segunda linha
+#A soma dos valores da diagonal
+#O maior valor digitado
 # 0 [ ] [ ] [ ]
 # 1 [ ] [ ] [ ]
 # 2 [ ] [ ] [ ]
 #    0   1   2
 
-matriz = []
-soma_pares = 0
-soma_colunas = 0
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+soma_total = 0
+numeros_pares = 0
+maior = None
+soma_diagonal = 0
 
 for linha in range(3):
-    linha_matriz = []
-    
     for coluna in range(3):
-        numero = int(input(f"Digite: [{linha}] [{coluna}]: "))
-        linha_matriz.append(numero)
-        if numero % 2 == 0:
-            soma_pares += numero
-    matriz.append(linha_matriz)
+        while True:
+            valor = input(f"Digite [{linha}][{coluna}]")
+        
+            if valor == '':
+                print("Não é permitido espaço vazio")
+            elif not valor.isnumeric():
+                print("Apenas números")
+            else:
+                numero = int(valor)
+                matriz[linha][coluna] = numero
+                soma_total += numero
+
+                
+                if numero % 2 == 0:
+                    numeros_pares += 1
+                    
+                if linha == coluna:
+                    soma_diagonal += numero
+                    
+                if maior is None or numero > maior:
+                    maior = numero
+                    
+                break
+                    
+print("-"*30)
+print()
+
+for linha in matriz:
+    print(linha)
     
-print("\nMatriz 3x3")
-for linha_matriz in matriz:
-    print(linha_matriz)
+print("-"*30)
+print()
 
-print(f"A soma dos números pares digitados: {soma_pares}")
-print(f"A soma dos valores da terceira coluna: ")
+print(f"Soma total dos números digitados: {soma_total}")
+print(f"A soma dos números pares: {numeros_pares}")
+print(f"A soma dos números em diagonal: {soma_diagonal}")
+print(f"O maior número digitado: {maior}")
+    
+                
+                
+        
+        
+            
 
+    
