4d7d9a7bfbdfcd75528dc62bb74f002f28a1861d
Clevison <cleverson-altF4>
2026-02-17 00:23:55 -0500

    minha matriz

--- Módulo 3  -Gustavo Guanabara/listas parte 2 - Variáveis compostas/Matriz-3-desafio.py
+++ Módulo 3  -Gustavo Guanabara/listas parte 2 - Variáveis compostas/Matriz-3-desafio.py
@@ -1,72 +1,57 @@
-# Tamanho da matriz
-while True:
-    tamanho_input = input("Digite o tamanho da matriz (ex: 3 para 3x3): ")
+tamanho = int(input("Digite uma Matriz 3x3: "))
+
+matriz = []
+
+for i in range(tamanho):
+    linha = []
+    for j in range(tamanho):
+        linha.append(0)
+    matriz.append(linha)
     
-    if tamanho_input.isnumeric() and int(tamanho_input) > 0:
-        tamanho = int(tamanho_input)
-        break
-    else:
-        print("Digite um número inteiro positivo.")
-
-# Criando matriz vazia
-matriz = [[0 for _ in range(tamanho)] for _ in range(tamanho)]
-
 soma_total = 0
-soma_terceira_coluna = 0
-maior_segunda_linha = None
 numeros_impares = 0
+soma_terceira_linha = 0
+maior_segunda_linha = 0
 
-# Preenchimento
 for linha in range(tamanho):
     for coluna in range(tamanho):
-        while True:
-            valor = input(f"Digite [{linha}][{coluna}]: ")
+        numero = int(input(f"Digite [{linha}][{coluna}]: "))
+        matriz[linha][coluna] = numero
+        soma_total += numero
+        
+        if numero % 2 == 1:
+            numeros_impares += 1
             
-            if valor == '':
-                print("Não é permitido espaço vazio")
-            elif not valor.lstrip('-').isnumeric():
-                print("É permitido apenas números inteiros")
+        if linha == 2:
+            soma_terceira_linha += numero
+            
+        if coluna == 1:
+            if linha == 0:
+                maior_segunda_linha += numero
             else:
-                numero = int(valor)
-                matriz[linha][coluna] = numero
-                soma_total += numero
-                
-                # Ímpares
-                if numero % 2 != 0:
-                    numeros_impares += 1
-                
-                # Terceira coluna (índice 2)
-                if coluna == 2:
-                    soma_terceira_coluna += numero
-                
-                # Segunda linha (índice 1)
-                if linha == 1:
-                    if maior_segunda_linha is None or numero > maior_segunda_linha:
-                        maior_segunda_linha = numero
-                
-                break
-
-print("-" * 40)
-
-# Mostrar matriz formatada
-for linha in matriz:
-    print("[", end="")
-    for numero in linha:
-        print(f"{numero:^5}", end="")
-    print("]")
-
-print("-" * 40)
-
-print(f"Soma total: {soma_total}")
+                if numero > maior_segunda_linha:
+                    maior_segunda_linha = numero
+                    
+print("-"*30)
+for linha in range(tamanho):
+    for coluna in range(tamanho):
+        print(matriz[linha][coluna], end=' ')
+    print()
+    
+print("-"*30)
+print(f"A soma total = {soma_total}")
 
 if tamanho > 2:
-    print(f"Soma da terceira coluna: {soma_terceira_coluna}")
+    print(f"A soma da terceira linha: {soma_terceira_linha}")
 else:
-    print("Não existe terceira coluna.")
+    print("Não existe")
 
 if tamanho > 1:
-    print(f"Maior valor da segunda linha: {maior_segunda_linha}")
+    print(f"O maior valor da segunda linha: {maior_segunda_linha}")
 else:
-    print("Não existe segunda linha.")
+    print("Não existe")
 
-print(f"Quantidade de números ímpares: {numeros_impares}")
+if numeros_impares == 0:
+    print("Não há números ímpares")
+else:
+    print(f"Números ímpares = {numeros_impares}")
