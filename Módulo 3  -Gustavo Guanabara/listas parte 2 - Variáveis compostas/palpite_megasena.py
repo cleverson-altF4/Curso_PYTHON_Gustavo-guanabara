3475eab58d9ed6bf2096afb1062f8559669e67f0
Clevison <cleverson-altF4>
2026-02-17 16:31:23 -0500

    números da Mega Sena

---
+++ Módulo 3  -Gustavo Guanabara/listas parte 2 - Variáveis compostas/palpite_megasena.py
@@ -0,0 +1,30 @@
+#Faça um programa que ajude um jogador da Mega Sena a criar palpite.
+#O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo cadastrando tudo em uma lista composta.
+
+from random import randint
+
+lista = []
+jogos = []
+
+quantidade_jogos = int(input("Quantos jogos: "))
+total = 1
+
+while total <= quantidade_jogos:
+    contador = 0
+    while True:
+        numero = randint(1,60)
+        
+        if numero not in lista:
+            lista.append(numero)
+            contador += 1
+            
+        if contador >= 6:
+            break
+        
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
+
+print(jogos)
+    
