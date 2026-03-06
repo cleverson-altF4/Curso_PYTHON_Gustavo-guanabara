4d9db759ce239d26ac40f75b7025e58d5925fa16
Clevison <cleversonmaiden034@gmail.com>
2026-03-06 16:03:45 -0500

    introduçao 2

---
+++ Módulo 3  -Gustavo Guanabara/dicionário/aula-guanabara-2.py
@@ -0,0 +1,27 @@
+#Laços
+pessoas = {'nome': 'Clevison', 'idade': 31, 'sexo': 'M', 'Nacionalidade': 'Brasil'}
+
+for chaves in pessoas:
+    print(f"{chaves}:")
+    
+print("-"*50)
+
+for chaves in pessoas.keys():
+    print(chaves)
+    
+print("-"*50)
+
+for chaves in pessoas.values():
+    print(chaves)
+    
+print("-"*50)
+
+for chaves in pessoas.items():
+    print(chaves)
+    
+print("-"*50)
+
+#Em vez de usar enumerate usa-se assim
+for chaves, valores in pessoas.items():
+    print(f"{chaves}: {valores}")
+    
