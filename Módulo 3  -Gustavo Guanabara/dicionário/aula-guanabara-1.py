5f51f38fcaf6af0cbfa9577343153de1dbf21e38
Clevison <cleversonmaiden034@gmail.com>
2026-03-06 15:48:29 -0500

    introdução

---
+++ Módulo 3  -Gustavo Guanabara/dicionário/aula-guanabara-1.py
@@ -0,0 +1,24 @@
+pessoas = {'nome': 'Clevison', 'idade': 31, 'sexo': 'M', 'Nacionalidade': 'Brasil'}
+
+print(pessoas)
+print("-"*50)
+
+#------------------------------------
+
+print(pessoas.keys())
+print("-"*50)
+
+#------------------------------------
+print(pessoas['nome'])
+print("-"*50)
+
+#------------------------------------
+
+print(f"Nome: {pessoas['nome']}  | idade: {pessoas['idade']} | sexo: {pessoas['sexo']} | Nacionalidade: {pessoas['Nacionalidade']}")
+print("-"*50)
+
+#------------------------------------
+print(pessoas.values())
+print("-"*50)
+
+print(pessoas.items())
