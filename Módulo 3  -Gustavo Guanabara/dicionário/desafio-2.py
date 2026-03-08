59560246310da3f919ee355a273e812ab129153b
Clevison <cleversonmaiden034@gmail.com>
2026-03-07 21:37:25 -0500

    segunda parte

--- Módulo 3  -Gustavo Guanabara/dicionário/desafio-2.py
+++ Módulo 3  -Gustavo Guanabara/dicionário/desafio-2.py
@@ -1,3 +1,18 @@
 #Crie um programa que onde 4 jogadores joguem um dado e tenham resultados.
 #Guarde em um dicionário
 #No final coloque esse dicionário em ordem sabendo que o vencedor tirou o maior número do dado
+
+from random import randint
+
+jogadores = {}
+
+for i in range(1, 5):
+    dado = randint(1, 6)
+    jogadores[f"Jogador {i}"] = dado
+    
+print("-"*30)
+print("    Resultados   ")
+print("-"*30)
+
+for jogador, res in jogadores.items():
+    print(f"{jogador} = {res}")
