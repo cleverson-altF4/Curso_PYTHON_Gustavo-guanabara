16f39d861bb254ad4946ecef0d192be64928a72c
Clevison <cleversonmaiden034@gmail.com>
2026-03-07 21:25:28 -0500

    primeira parte do desafio

---
+++ Módulo 3  -Gustavo Guanabara/dicionário/desafio-2.py
@@ -0,0 +1,3 @@
+#Crie um programa que onde 4 jogadores joguem um dado e tenham resultados.
+#Guarde em um dicionário
+#No final coloque esse dicionário em ordem sabendo que o vencedor tirou o maior número do dado
