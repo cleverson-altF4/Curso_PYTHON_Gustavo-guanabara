0e4ad95eb8dccb9d35e5e0fac6c81644df18e7d0
Clevison <cleversonmaiden034@gmail.com>
2026-03-07 19:25:34 -0500

    dúvida

---
+++ Módulo 3  -Gustavo Guanabara/dicionário/aula-guanabara-3.py
@@ -0,0 +1,13 @@
+lista = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'Rj'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'Sp'}
+
+lista.append(estado1)
+lista.append(estado2)
+
+
+print(lista[0]['uf'])
+print(lista[0]['sigla'])
+
+print(lista[1]['uf'])
+print(lista[1]['sigla'])
