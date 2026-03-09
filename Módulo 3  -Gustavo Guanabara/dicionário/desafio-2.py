90a4e0607f10c2ced2a8abba7e874d5db53fdfaf
Clevison <cleversonmaiden034@gmail.com>
2026-03-08 21:23:56 -0500

    não resolvido

--- Módulo 3  -Gustavo Guanabara/dicionário/desafio-2.py
+++ Módulo 3  -Gustavo Guanabara/dicionário/desafio-2.py
@@ -3,16 +3,18 @@
 #No final coloque esse dicionário em ordem sabendo que o vencedor tirou o maior número do dado
 
 from random import randint
+from time import sleep
+jogador = {}
 
-jogadores = {}
-
-for i in range(1, 5):
-    dado = randint(1, 6)
-    jogadores[f"Jogador {i}"] = dado
+for i in range(4):
+    jogador = randint(1,6)
+    print(f"O jogador - {i+1} tirou {jogador}")
+    sleep(0.7)
     
 print("-"*30)
-print("    Resultados   ")
-print("-"*30)
+print("   Ranking dos jogadores   ")
 
-for jogador, res in jogadores.items():
-    print(f"{jogador} = {res}")
+
+
+    
+    
