8bb501c823e0b9c559da0eb515f5c36662e05a51
Clevison <cleversonmaiden034@gmail.com>
2026-03-08 22:26:14 -0500

    melhorias

--- Módulo 3  -Gustavo Guanabara/dicionário/desafio-2.py
+++ Módulo 3  -Gustavo Guanabara/dicionário/desafio-2.py
@@ -2,19 +2,22 @@
 #Guarde em um dicionário
 #No final coloque esse dicionário em ordem sabendo que o vencedor tirou o maior número do dado
 
+
+
 from random import randint
 from time import sleep
+from operator import itemgetter
+
 jogador = {}
 
-for i in range(4):
-    jogador = randint(1,6)
-    print(f"O jogador - {i+1} tirou {jogador}")
-    sleep(0.7)
+for i in range(1, 5):
+    jogador[f'Jogador {i}'] = randint(1, 6)
+    print(f"O jogador {i} tirou {jogador[f'Jogador {i}']}")
     
 print("-"*30)
-print("   Ranking dos jogadores   ")
+print("   Ranking   ")
 
+ranking = sorted(jogador.items(), key=itemgetter(1), reverse=True)
 
-
-    
-    
+for posicao, valor in enumerate(ranking):
+    print(f"{posicao}º lugar {valor}")
