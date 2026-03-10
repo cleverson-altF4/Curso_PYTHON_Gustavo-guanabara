03f42ea04762ee383d0c0bdf6b3d903693bebca9
Clevison <cleversonmaiden034@gmail.com>
2026-03-09 22:25:49 -0500

    desa

--- Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-2.py
+++ Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-2.py
@@ -1,3 +1,11 @@
 #Crie um programa que onde 4 jogadores joguem um dado e tenham resultados.
 #Guarde em um dicionário
 #No final coloque esse dicionário em ordem sabendo que o vencedor tirou o maior número do dado
+
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+
+aluno = {}
+
