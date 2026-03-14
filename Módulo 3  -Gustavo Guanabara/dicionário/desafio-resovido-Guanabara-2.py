fa6d2cd7537e349ec99aff061124684c244f90e5
Clevison <cleversonmaiden034@gmail.com>
2026-03-14 18:15:02 -0500

    aprendizado

--- Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-2.py
+++ Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-2.py
@@ -1,50 +1,38 @@
-#Crie um programa que onde 4 jogadores joguem um dado e tenham resultados.
-#Guarde em um dicionário
-#No final coloque esse dicionário em ordem sabendo que o vencedor tirou o maior número do dado
-
 from random import randint
-from time import sleep
 from operator import itemgetter
+from time import sleep
 import emoji
 
-
-jogador = {
+jogo = {
     'jogador 1': randint(1,6),
     'jogador 2': randint(1,6),
     'jogador 3': randint(1,6),
     'jogador 4': randint(1,6)
 }
 
+rankink = {}
 
-ranking = {}
-
-for k, v in jogador.items():
-    print(f"{k} tirou {v}")
+for k, c in jogo.items():
+    print(f"{k}: {c}")
     sleep(1)
     
-print("-"*30)
-print("     Ranking     ")
-ranking = sorted(jogador.items(), key=itemgetter(1), reverse=True)
+rankink = sorted(jogo.items(), key=itemgetter(1), reverse=True)
 
-for i, valor in enumerate(ranking):
-    print(f"{i+1}° colocado {valor[0]}: {valor[1]}")
-    sleep(1)
     
-print("-"*30)
-print("  Lista de jogadores   ")
-
-for i in ranking:
+print(f"   resultado    ")
+for i in rankink:
     if i[1] == 1:
-        print(f"{i[0]} tirou 1")
+        print(f"{i[0]} tirou {i[1]}")
     elif i[1] == 2:
-        print(f"{i[0]} tirou 2")
+        print(f"{i[0]} tirou {i[1]}")
     elif i[1] == 3:
-        print(f"{i[0]} tirou 3")
+        print(f"{i[0]} tirou {i[1]}")
     elif i[1] == 4:
-        print(f"{i[0]} tirou 4")
+        print(f"{i[0]} tirou {i[1]}")
     elif i[1] == 5:
-        print(f"{i[0]} tirou 5")
+        print(f"{i[0]} tirou {i[1]}")
     elif i[1] == 6:
         print(emoji.emojize(f"O vencedor foi o {i[0]} :trophy:", language='alias'))
     else:
-        print("Não teve vencedor")
+        print("Ninguém jogou o dado")
+        
