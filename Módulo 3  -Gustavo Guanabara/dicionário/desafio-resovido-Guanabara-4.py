d5e147c0848930c161417ce06c2220000470e11e
Clevison <cleversonmaiden034@gmail.com>
2026-03-17 17:41:59 -0500

    finalizado sem copiar

--- Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-4.py
+++ Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-4.py
@@ -7,6 +7,19 @@ for i in range(jogador['partidas']):
     gol.append(int(input(f"Quantos gols {jogador['nome']} fez? ")))
     
 jogador['gol'] = gol
+jogador['total'] = sum(gol)
 
 print(jogador)
 
+print("-"*35)
+
+for k, v in jogador.items():
+    print(f"{k}: ------ {v}")
+    
+print("-"*35)
+
+print(f"O jogador {jogador['nome']} jogou {(jogador['partidas'])} partidas")
+
+for i, valor in enumerate(jogador['gol']):
+    print(f" Na partida {i+1} fez {valor} gols")
+print(f"Foi um total de {sum(jogador['gol'])} gols")
