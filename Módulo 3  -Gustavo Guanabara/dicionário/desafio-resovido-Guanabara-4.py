04833432622dd2a5ec579840d1bfba60f30bc7fc
Clevison <cleversonmaiden034@gmail.com>
2026-03-19 15:50:50 -0500

    melhorias

--- Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-4.py
+++ Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-4.py
@@ -1,25 +1,28 @@
 jogador = {}
 gol = []
-jogador['nome'] = str(input("Nome do jogador: ")).strip()
-jogador['partidas'] = int(input(f"Quantas partidas {jogador['nome']} jogou? "))
+jogador['Nome'] = str(input("Nome do jogador: ")).strip()
+jogador['partidas'] = int(input(f"Quantos partidas {jogador['Nome']} fez? "))
 
 for i in range(jogador['partidas']):
-    gol.append(int(input(f"Quantos gols {jogador['nome']} fez? ")))
+    gol.append(int(input(f"Quantos gols {jogador['Nome']} fez? ")))
     
-jogador['gol'] = gol
+jogador['gols'] = gol
 jogador['total'] = sum(gol)
 
-print(jogador)
+print('-'*40)
 
-print("-"*35)
-
-for k, v in jogador.items():
-    print(f"{k}: ------ {v}")
+for k, c in enumerate(jogador.items()):
+    print(f"{k+1} ________ {c}")
     
-print("-"*35)
+print("-"*40)
 
-print(f"O jogador {jogador['nome']} jogou {(jogador['partidas'])} partidas")
+for i in jogador.items():
+    print(f"{i}")
+    
+print("-"*40)
 
-for i, valor in enumerate(jogador['gol']):
-    print(f" Na partida {i+1} fez {valor} gols")
-print(f"Foi um total de {sum(jogador['gol'])} gols")
+print(f"A quantidade de gols que {jogador['Nome']} fez foi {jogador['gols']}")
+print(f"O total de gols na temporada foi {jogador['total']} gols no ano.")
+
+for i, valor in enumerate(jogador['gols']):
+    print(f"{i+1} _______{valor} gols")
