ab9a20cd659d863cbabb1d64f31cdf0c7019a88a
Clevison <cleversonmaiden034@gmail.com>
2026-03-21 21:05:12 -0500

    aprendizado

--- Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-5.py
+++ Módulo 3  -Gustavo Guanabara/dicionário/desafio-resovido-Guanabara-5.py
@@ -1,33 +1,72 @@
-lista = []
-soma_idade = 0
-total_mulheres = 0
+#Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista.
+
+#No final mostre:
+# A - Quantas pessoas foram cadastradas
+
+# B - A média de idade 
+
+# C - Uma lista com mulheres
+
+# D - Uma lista com idade acima da média
+
+pessoa = {}
+galera = []
+soma = media = 0
+
 while True:
-    pessoa = {}
-    pessoa['Nome'] = str(input("Digite o seu nome: ")).strip() 
-    pessoa['Sexo'] = str(input("Digite o seu sexo: [M/F]: ")).strip().upper()
-    pessoa['idade'] = int(input("Digite a sua idade: "))
-    while pessoa['Sexo'] not in ['M', 'F']:
-        print("Digite corretamente apenas [M ou F]")
-        pessoa['Sexo'] = str(input("Digite novamente: ")).strip().upper() [0]
+    pessoa.clear() #Limpa o buffer
+    pessoa['nome'] = str(input("Digite o seu nome: ")).strip()
+    
+    #Valida se o sexo está corretamente e se não ele vira loop até digitar corretamente
+    while True:
+        pessoa['sexo'] = str(input("Digite o seu sexo: [M ou F]: ")).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        else:
+            print("Apenas M ou F")
+    #Valida a entrada de idade         
+    while True:
+        idade = input("Digite a sua idade: ")
         
-    #Unir a lista e dicionário
-    lista.append(pessoa.copy())
+        if idade.isdigit():
+            pessoa['idade'] = int(idade)
+            break
+        else:
+            print("Apenas números")
+            
+    galera.append(pessoa.copy()) #cópia 
     
-    total = len(lista)
-    soma_idade += pessoa['idade']
-    media = soma_idade / len(lista)
-    
-    if pessoa['Sexo'] == 'F':
-        total_mulheres += 1
-    
-    
-    continuar = str(input("Deseja continuar? "))[0]
-    if continuar == 'n':
+    soma += pessoa['idade'] #Soma todas as idades digitados
+            
+    while True:
+        resposta = str(input("Deseja continuar? aperte [S/N]: ")).upper()[0]
+        if resposta in 'SN':
+            break
+        print("Erro! tente novamente")   
+        
+    if resposta == 'N':
         break
 
-print(f"Total de pessoas: {total} ")
-print(f"A média de idade: {media:.2f}")
-print(f"Total de mulheres: {total_mulheres}")
+print("-"*35)
+print(f"A - Ao total foram cadastrados {len(galera)} pessoas")
+media = soma / len(galera)
+print(f"B - Média de idades do grupo {media} anos")
+print("C - Quantidade de mulheres cadastradas: ", end=' ')
 
-        
-      
+#laço para pegar a quantidade de mulheres
+for pessoa in galera:
+    if pessoa['sexo'] == 'F':
+        print(f"{pessoa['nome']}", end= ' | ')
+print()
+    
+print("-"*35)
+
+#Laço para pegar a média
+
+for pessoa in galera:
+    if pessoa['idade'] >= media:
+        print("  ", end='')
+        for keys, valor in pessoa.items():
+            print(f"{keys}: {valor} ", end= ' | ')
+print()
+            
