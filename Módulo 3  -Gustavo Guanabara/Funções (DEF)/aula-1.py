531acbd6a22d8954c7d81f3645ac34f1286d2beb
Clevison <cleversonmaiden034@gmail.com>
2026-04-04 16:55:36 -0500

    aula-1

---
+++ Módulo 3  -Gustavo Guanabara/Funções (DEF)/aula-1.py
@@ -0,0 +1,17 @@
+#Sem parâmetros
+def linha():
+    print("-"*30)
+    print("        Sem parâmetros     ")
+    
+linha()
+
+#Com parâmetros
+def titulo(msg):
+    print("-"*30)
+    print(msg)
+    print("-"*30)
+    
+#Programa principal    
+titulo('        Com parâmetros        ')
+titulo("Estou aprendendo funções")
+
