9eb43815f20852218c666cfa7d07a6635f059c9c
Clevison <cleversonmaiden034@gmail.com>
2026-04-12 12:04:51 -0500

    inicio

--- Módulo 3  -Gustavo Guanabara/Funções (DEF)/aula-6.py
+++ Módulo 3  -Gustavo Guanabara/Funções (DEF)/aula-6.py
@@ -5,3 +5,19 @@
 #A) de 1 até 10, de 1 em 1
 #B0 de 10 até 0, de 2 em 2
 #Uma contagem personalizado 
+from time import sleep
+
+inicio = int(input("Inicio: "))
+fim = int(input("Fim: "))
+passo = int(input("Passo: "))
+
+
+def contador(inicio, fim, passo):
+    for i in range(inicio, fim, passo):
+        print(f"{i}",end=' ')
+        sleep(0.5)
+        
+        
+        
+        
+contador(inicio, fim, passo)
