51a8239ca577728c9e88f454f018ce2b201a5433
Clevison <cleversonmaiden034@gmail.com>
2026-04-11 22:29:51 -0500

    exercicio resolvido 2

---
+++ Módulo 3  -Gustavo Guanabara/Funções (DEF)/aula-5.py
@@ -0,0 +1,9 @@
+#Faça um programa que tenha uma função chamada escreva(): que receba um texto qualquer como parametro e mostre uma mensagem com um tamanho adaptável 
+
+def escreva(texto):
+    print("-------------")
+    print(texto)
+    print("--------------")
+    
+    
+escreva('Olá, mundo!')
