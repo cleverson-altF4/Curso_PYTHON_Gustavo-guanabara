6540f2dd61c8ccb78d0d00b1bfda84886848603e
Clevison <cleversonmaiden034@gmail.com>
2026-04-11 22:37:03 -0500

    aula 3

---
+++ Módulo 3  -Gustavo Guanabara/Funções (DEF)/aula-6.py
@@ -0,0 +1,7 @@
+#faça um programa que tenha uma função chamada contador() que receba 3 parâmetros, INICIO, FIM, PASSO e realize uma contagem 
+
+#Seu programa tem que realizar 3 contagens através da função criada
+
+#A) de 1 até 10, de 1 em 1
+#B0 de 10 até 0, de 2 em 2
+#Uma contagem personalizado 
