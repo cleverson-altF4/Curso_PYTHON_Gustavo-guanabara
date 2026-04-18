61d32c7dc6ce82a8aed618ac02a8d366c0227510
Clevison <cleversonmaiden034@gmail.com>
2026-04-18 11:18:45 -0500

    update

--- Módulo 3  -Gustavo Guanabara/Funções (DEF)/empacotar em funções/exercicio-resolvido.py
+++ Módulo 3  -Gustavo Guanabara/Funções (DEF)/empacotar em funções/exercicio-resolvido.py
@@ -1,30 +1,31 @@
 from random import randint
 from time import sleep
 
+def linha():
+    print("-"*45)
 
 def sorteio(lista):
-    print("\nSorteando 5 números\n")
+    print("\n Somando números pares\n")
     for numero in range(5):
         numero = randint(1,10)
         lista.append(numero)
         print(numero, end=' ', flush=True)
-        sleep(1)
-    print(f"---- > Números selecionados: {lista}")
+        sleep(0.5)
+    print(f"------->: Números adicionados {lista}")
     
-    
+linha()   
 def somando_pares(lista):
     soma = 0
     
     for i in lista:
         if i % 2 == 0:
             soma += i
-    print(f"A soma dos {i} números pares selecionados: {soma}")
-            
-            
-lista_principal = []
+    print(f"Nùmeros pares da lista: {soma}")
+
+#programa principal    
+lista_principal= []
 
 sorteio(lista_principal)
+linha()
 somando_pares(lista_principal)
-            
-            
 
