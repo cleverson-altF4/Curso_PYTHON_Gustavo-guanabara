3c5efdacda04e3d06ed9d53f91fcbeb82b66f03f
Clevison <cleversonmaiden034@gmail.com>
2026-04-20 21:28:12 -0500

    desafiio concluído

--- Módulo 3  -Gustavo Guanabara/Funções parte 2/desafio-5.py
+++ Módulo 3  -Gustavo Guanabara/Funções parte 2/desafio-5.py
@@ -1,4 +1,11 @@
-def notas(*n, sit=False):
+def notas(*n, situacao=True):
+    """
+        A função para analisar a sintuação das notas, ele coloca como parâmetro:
+        param: *n
+        param: sintuacao=False ou True
+        dicionário: ele lê o total de notas, maior, menor, media e a sintuacao do aluno se encontra
+    """
+    
     dicionario = {
         "total": len(n),
         "maior": max(n),
@@ -6,6 +13,18 @@ def notas(*n, sit=False):
         "media": sum(n) / len(n)
     }
     
-    if sit:
+    if situacao:
         if dicionario['media'] >= 7:
-            sit=True
+            dicionario['situacao'] ='Boa'
+        elif dicionario['media'] >= 5:
+            dicionario['situacao']= 'Razoável'
+        elif dicionario['media'] < 5:
+            dicionario['situacao']= 'Ruim'
+    return dicionario
+            
+            
+resposta = notas(5.5,6.4,11)
+print(resposta)
+
+help(notas)
+    
