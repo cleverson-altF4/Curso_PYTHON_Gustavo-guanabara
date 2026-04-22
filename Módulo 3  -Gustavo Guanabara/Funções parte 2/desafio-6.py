8909334b2d7f224ce0bae7ca9301e617e2b2fd59
Clevison <cleversonmaiden034@gmail.com>
2026-04-22 14:31:39 -0500

    update de ajustes

--- Módulo 3  -Gustavo Guanabara/Funções parte 2/desafio-6.py
+++ Módulo 3  -Gustavo Guanabara/Funções parte 2/desafio-6.py
@@ -5,9 +5,10 @@ cores = ['\033[31m', '\033[32m', '\033[m']
 
 def titulo(msg, cor=0):
     tamanho = len(msg) + 4
-    print("~"* tamanho)
-    print(f"  {msg}   ")
-    print(cores[0])
+    print(cores[cor], end='')
+    print("~"* tamanho) 
+    print(f"  {msg}  ")
+    print("~"* tamanho) 
     print(cores[2])
     
     
