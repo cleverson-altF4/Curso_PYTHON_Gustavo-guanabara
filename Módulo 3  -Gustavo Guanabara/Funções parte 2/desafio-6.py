46c0662f274d64933cd79ba750cddd046455ecb9
Clevison <cleversonmaiden034@gmail.com>
2026-04-23 21:48:17 -0500

    update concluído

--- Módulo 3  -Gustavo Guanabara/Funções parte 2/desafio-6.py
+++ Módulo 3  -Gustavo Guanabara/Funções parte 2/desafio-6.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 cores = ['\033[31m', '\033[32m', '\033[m']
 fundo_verde = ['\033[42m']
 
@@ -7,14 +9,15 @@ def titulo(msg, cor=0):
     print("-"* tamanho) 
     print(f"  {msg}  ")
     print("-"* tamanho) 
-    print(cores[2])   
+    print(cores[2]) 
+    sleep(1)  
     
 def ajuda(com_ajuda):
     titulo(f"Acessando o manual do comando {com_ajuda}", cor=1 )
     print(fundo_verde[0], end='')
     help(com_ajuda)
     print(cores[2])
-
+    
 while True:
    titulo("SISTEMA DE AJUDA PYHELP", cor=0)
    
