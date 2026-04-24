bd27eef325b88f8a2341f68a94329f1c20647214
Clevison <cleversonmaiden034@gmail.com>
2026-04-23 21:44:22 -0500

    projeto conclúido

--- Módulo 3  -Gustavo Guanabara/Funções parte 2/desafio-6.py
+++ Módulo 3  -Gustavo Guanabara/Funções parte 2/desafio-6.py
@@ -1,18 +1,17 @@
-#tupla para pegar as cores do ANSI
-
 cores = ['\033[31m', '\033[32m', '\033[m']
+fundo_verde = ['\033[42m']
 
 def titulo(msg, cor=0):
     tamanho = len(msg) + 4
     print(cores[cor], end='')
-    print("~"* tamanho) 
+    print("-"* tamanho) 
     print(f"  {msg}  ")
-    print("~"* tamanho) 
+    print("-"* tamanho) 
     print(cores[2])   
     
 def ajuda(com_ajuda):
-    print(cores[1])
     titulo(f"Acessando o manual do comando {com_ajuda}", cor=1 )
+    print(fundo_verde[0], end='')
     help(com_ajuda)
     print(cores[2])
 
@@ -21,7 +20,7 @@ while True:
    
    usuario = input(">>> ").strip()
    if usuario.lower() == 'fim':
+       titulo("Projeto concluído", cor=1)
        break
    else:
-       print(cores[2])
        ajuda(usuario)
