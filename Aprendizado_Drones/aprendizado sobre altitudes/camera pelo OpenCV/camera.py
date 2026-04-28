abb4cbd16c336c5c49c7ffbe294ef0eb570d9cfb
Clevison <cleversonmaiden034@gmail.com>
2026-04-28 17:18:09 -0500

    update

--- Aprendizado_Drones/aprendizado sobre altitudes/camera pelo OpenCV/camera.py
+++ Aprendizado_Drones/aprendizado sobre altitudes/camera pelo OpenCV/camera.py
@@ -11,9 +11,15 @@ if not camera.isOpened():
     print("A câmera não foi encontrada!")
     
 while True:
-    ok, frame = camera.read()
+    ok, frame = camera.read() #Aqui ele abre a câmera viu carai
     if not ok:
-       break 
+        break # Se não abriu ele para.
+    altura, largura, _ = frame.shape # O _ é um underscore simples
+    cx = largura // 2
+    cy = altura // 2
+    
+    cv2.line(frame, (0,cy),(largura, cy), (0,0,255),1) #Linha horizontal
+    cv2.line(frame, (cx, 0), (cx, altura), (0,0,255), 1)
     
     #cv2.putText(frame, "texto", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     
@@ -31,10 +37,7 @@ while True:
         
     cv2.putText(frame, f"Status: {status}", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
-    
-    
     cv2.imshow("HUD CAMERA", frame)
-    
     
     if cv2.waitKey(1) == ord('q'):
         break
