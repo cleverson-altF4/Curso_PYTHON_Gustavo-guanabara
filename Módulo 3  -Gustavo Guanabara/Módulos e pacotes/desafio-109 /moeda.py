865ea9d9076665d713b62e6d534e68eb7d766cdd
Clevison <cleversonmaiden034@gmail.com>
2026-04-29 14:52:27 -0500

    updade

---
+++ Módulo 3  -Gustavo Guanabara/Módulos e pacotes/desafio-109 /moeda.py
@@ -0,0 +1,22 @@
+def aumentar(preco=0, taxa=0):
+    resultado = preco + (preco * taxa / 100)
+    return resultado
+
+
+def diminuir(preco=0, taxa=0):
+    resultado = preco - (preco * taxa / 100)
+    return resultado
+
+
+def dobro(preco=0):
+    resultado = preco * 2
+    return resultado
+
+
+def metade(preco):
+    resultado = preco / 2
+    return resultado 
+
+
+def dinheiro(preco=0, cedula="R$"):
+    return f"{cedula}{preco:.2f}".replace(".", ",")
