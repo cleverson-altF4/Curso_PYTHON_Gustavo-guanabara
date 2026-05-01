19c31589f2ad49de75f381c754eda101f91daa78
Clevison <cleversonmaiden034@gmail.com>
2026-04-30 21:57:58 -0500

    update

--- Módulo 3  -Gustavo Guanabara/Módulos e pacotes/desafio-109 /moeda.py
+++ Módulo 3  -Gustavo Guanabara/Módulos e pacotes/desafio-109 /moeda.py
@@ -1,19 +1,19 @@
-def aumentar(preco=0, taxa=0):
+def aumentar(preco=0, taxa=0, formato= False):
     resultado = preco + (preco * taxa / 100)
-    return resultado
+    return resultado if formato is False else moeda(resultado)
 
 
-def diminuir(preco=0, taxa=0):
+def diminuir(preco=0, taxa=0, formato= False):
     resultado = preco - (preco * taxa / 100)
     return resultado
 
 
-def dobro(preco=0):
+def dobro(preco=0, formato = False):
     resultado = preco * 2
     return resultado
 
 
-def metade(preco):
+def metade(preco=0, Formato = False):
     resultado = preco / 2
     return resultado 
 
