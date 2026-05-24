899957420214b01a19e4d66149bfc835444780f6
Clevison <cleversonmaiden034@gmail.com>
2026-05-24 15:41:10 -0500

    update

--- Módulo 3  -Gustavo Guanabara/Tratamento de erros - Try e exception/desafio-2.py
+++ Módulo 3  -Gustavo Guanabara/Tratamento de erros - Try e exception/desafio-2.py
@@ -1,17 +1,17 @@
-import urllib.request
+from urllib import request
 
 def verificar_site(url):
     try:
-        # Tenta abrir o site informado
-        site = urllib.request.urlopen(url)
-    except urllib.error.URLError:
-        # Caso ocorra um erro de conexão (site offline ou sem internet)
+        site = request.urlopen(url)
+        
+    except request.HTTPError:
+    
         print('\033[31mO site não está acessível no momento. :(\033[m')
     else:
-        # Se o site abrir com sucesso
+
         print('\033[32mConsegui acessar o site com sucesso! :)\033[m')
-        # Opcional: imprimir o conteúdo html do site
-        # print(site.read()) 
+        
+        return site
 
 # Execução
-verificar_site('http://www.pudim.com.br')
+verificar_site('https://www.youtube.com/')
