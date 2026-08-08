1699528d94b618379ca27d4fce9439f6139f8363
clevison <cleversonmaiden031@gmail.com>
2026-08-07 22:20:08 -0300

    aula--2 python

--- Módulo 4 - Gustavo Guanabara/POO /aula-2.py
+++ Módulo 4 - Gustavo Guanabara/POO /aula-2.py
@@ -1,8 +1,29 @@
-class ContaBancaria:
-    def __init__(self, titular, saldo = 0,):
+class contaBancaria:
+    def __init__(self, titular):
         self.titular = titular
-        self.saldo = saldo
+        self.saldo = 0
 
 
-    def depositar(valor):
-        
+    def Depositar(self, valor):
+        self.saldo = self.saldo + valor
+        print(f"Você depositou R$ {valor} reais")
+
+
+    def sacar(self, valor):
+        if valor >= valor:
+            self.saldo = self.saldo - valor
+            print(f"O valor sacado foi de R$ {valor} reais")
+        else:
+            print("Saldo insuficiente")
+
+
+    def MostrarSaldo(self):
+        print(f"Titular: {self.titular}  | saldo: {self.saldo} reais")
+
+
+
+conta = contaBancaria("Clevison")
+conta.Depositar(250)
+conta.sacar(50)
+conta.Depositar(1500)
+conta.MostrarSaldo()
