3aa311441e22b21501cb0c064db1f1a37ba11115
clevison <cleversonmaiden031@gmail.com>
2026-08-29 12:16:32 -0300

    up

--- Módulo 4 - Gustavo Guanabara/Desafios-POO/desafio-4.py
+++ Módulo 4 - Gustavo Guanabara/Desafios-POO/desafio-4.py
@@ -6,17 +6,21 @@ class Livro:
         self.paginas = paginas
         self.total = 1
 
-        print(f"Você acabou de abrir o livro {self.titulo} que tem {self.paginas} páginas no total. Você está "
-              f"agora na página {self.total}")
+        print(f"Você acabou de abrir o livro {self.titulo} que tem {self.paginas} páginas no total.")
 
     def avancarPaginas(self, total):
         for i in range(total):
-            print(f"pag{i+1}", end=' ')
-            sleep(1)
-            total += 1
+
+            if i >= self.paginas:
+                print(f"O livro termina na página {self.paginas}", end= " ")
+                break
+            else:
+                print(f"pag {i + 1}", end=' ')
+                sleep(1)
+                total += 1
+
         print(f"Você avançou {i+1} e agora está na página {total}")
 
 
 li1 = Livro("Harry Potter", 20)
-li1.avancarPaginas(5)
 li1.avancarPaginas(5)
