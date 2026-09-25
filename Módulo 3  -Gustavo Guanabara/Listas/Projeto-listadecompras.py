d8fd48182d9cf0098a11f7e648c61a4a507dcd94
clevison <cleversonmaiden031@gmail.com>
2026-09-24 23:42:39 -0300

    updadte

--- Módulo 3  -Gustavo Guanabara/Listas/Projeto-listadecompras.py
+++ Módulo 3  -Gustavo Guanabara/Listas/Projeto-listadecompras.py
@@ -1,7 +1,60 @@
 """Lista de compras interativa"""
 
-
 lista = []
+def documentacao():
+    """crie outro enunciado lista = []
+
+    while True:
+    print("*"*40)
+    print("~"*12, "Lista de compras", "~"*12)
+    print('''
+        [1] - Adicionar produto na lista
+        [2] - Remover o produto da lista
+        [3] - Mostrar os produtos da lista
+        [4] - Sair do programa''')
+    opcao = int(input("Selecione a opção desejada: "))
+
+    if opcao == 1:
+        produto = str(input("Nome do produto da lista: ")).strip()
+
+        if produto == '':
+            print("\nNão é permitido espaços vazios")
+        elif produto in lista:
+            print("O produto está na lista")
+        else:
+            lista.append(produto)
+            print("\nProduto adicionado com sucesso!")
+
+    elif opcao == 2:
+        produto = str(input("Qual produto deseja remover? ")).strip()
+
+        for posicao, mercadoria in enumerate(lista):
+            if mercadoria == produto:
+                print("\nRemovendo produto da lista")
+                lista.remove(mercadoria)
+                print(f"{posicao + 1} = {mercadoria} removido ")
+                break
+        else:
+            print("Este produto não está na lista")
+
+    elif opcao == 3:
+        if len(lista) == 0:
+            print("A lista está vazia")
+        else:
+            for posicao, mercadoria in enumerate(lista):
+                print(f"{posicao + 1} = {mercadoria}")
+
+    elif opcao == 4:
+        print("\nTabela completa da lista de compras\n")
+
+        if len(lista) == 0:
+            print("A lista está vazia")
+        else:
+            for posicao, mercadoria in enumerate(lista):
+                print(f"{posicao + 1} = {mercadoria}")
+                break"""
+
+
 def Menu():
     print("*"*50)
     print('''
@@ -9,6 +62,7 @@ def Menu():
         [2] - Remover o produto da lista
         [3] - Mostrar os produtos da lista
         [4] - Sair do programa
+        [5] - Ler a documentação
     ''')
     print("*" * 50)
 
@@ -52,4 +106,13 @@ while True:
             print(f"{posicao + 1} = {mercadoria}")
         break
 
+    if opcao == 5:
+        print("Documentação antiga")
+        print(documentacao.__doc__)
+        continue
+
     escolher(opcao)
+
+
+
+
